52f074009a4e5abc6af4a75461a0145223c32b96
Ursulahoney <kaisereliz@gmail.com>
2026-01-31 07:47:28 -0700

    Add Eleventy configuration for file passthrough
    Configure Eleventy to copy specified files and set directories.

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("data");
+  eleventyConfig.addPassthroughCopy("scrapers");
+  eleventyConfig.addPassthroughCopy("styles.css");
+  eleventyConfig.addPassthroughCopy("app.js");
+  eleventyConfig.addPassthroughCopy("Circular-badge-logo.png");
+  eleventyConfig.addPassthroughCopy("eos-logo.png");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    }
+  };
+};
